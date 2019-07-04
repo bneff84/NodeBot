9b84936e9c3f83115bed4eca378d183598799abf
Brian Neff <bneff84@gmail.com>
2019-07-03 20:43:40 -0400

    Added basic Networking
    Added basic network handler to listen for JSON messages over port 1337.
    Added event dispatch handling to IO class for network messages.
    Created standard "action" configuration for now until DB functionality is in place.
    Cleaned up some console log messages.
    Moved testing automation to new file nested in lib/automations where "bot" configurations can reside until DB functionality is implemented.

--- actions.js
+++ actions.js
@@ -1,6 +1,215 @@
+/**
+ * @type {io}
+ */
+let io = require('./lib/io.js');
+
 module.exports = {
-    actionOne: {
+    /*
+     * Targeting
+     */
+    targetSelf: {
+        type: 'keypress',
+        keys: 'F1'
+    },
+    targetGroupMember1: {
+        type: 'keypress',
+        keys: 'F2'
+    },
+    targetGroupMember2: {
+        type: 'keypress',
+        keys: 'F3'
+    },
+    targetGroupMember3: {
+        type: 'keypress',
+        keys: 'F4'
+    },
+    targetGroupMember4: {
+        type: 'keypress',
+        keys: 'F5'
+    },
+    targetGroupMember5: {
+        type: 'keypress',
+        keys: 'F6'
+    },
+    targetExtendedTarget1: {
+        type: 'keycombination',
+        keys: ['control','F1']
+    },
+    targetExtendedTarget2: {
+        type: 'keycombination',
+        keys: ['control','F1']
+    },
+    targetExtendedTarget3: {
+        type: 'keycombination',
+        keys: ['control','F1']
+    },
+    targetExtendedTarget4: {
+        type: 'keycombination',
+        keys: ['control','F1']
+    },
+    targetExtendedTarget5: {
+        type: 'keycombination',
+        keys: ['control','F1']
+    },
+    targetMainGroupMember: {
+        type: 'keypress',
+        keys: '='
+    },
+    assist: {
+        type: 'keypress',
+        keys: '1',
+    },
+
+    /*
+     * Movement
+     */
+    moveForwardDown: {
+        type: 'keysequence',
+        keys: [
+            ['w', io.EVENT_KEY_DOWN]
+        ]
+    },
+    moveForwardUp: {
+        type: 'keysequence',
+        keys: [
+            ['w', io.EVENT_KEY_UP]
+        ]
+    },
+    moveBackwardDown: {
+        type: 'keysequence',
+        keys: [
+            ['s', io.EVENT_KEY_DOWN]
+        ]
+    },
+    moveBackwardUp: {
+        type: 'keysequence',
+        keys: [
+            ['s', io.EVENT_KEY_UP]
+        ]
+    },
+    moveLeftDown: {
+        type: 'keysequence',
+        keys: [
+            ['a', io.EVENT_KEY_DOWN]
+        ]
+    },
+    moveLeftUp: {
+        type: 'keysequence',
+        keys: [
+            ['a', io.EVENT_KEY_UP]
+        ]
+    },
+    moveRightDown: {
+        type: 'keysequence',
+        keys: [
+            ['d', io.EVENT_KEY_DOWN]
+        ]
+    },
+    moveRightUp: {
+        type: 'keysequence',
+        keys: [
+            ['d', io.EVENT_KEY_UP]
+        ]
+    },
+    moveJump: {
+        type: 'keypress',
+        keys: 'space'
+    },
+    moveSitToggle: {
+        type: 'keycombination',
+        keys: ['control', 's']
+    },
+    moveCrouchToggle: {
+        type: 'keypress',
+        keys: 'x'
+    },
+    followTarget: {
+        type: 'keypress',
+        keys: '-'
+    },
+
+    /*
+     * Abilities
+     */
+    ability1: {
         type: 'keypress',
         keys: '1'
+    },
+    ability2: {
+        type: 'keypress',
+        keys: '2'
+    },
+    ability3: {
+        type: 'keypress',
+        keys: '3'
+    },
+    ability4: {
+        type: 'keypress',
+        keys: '4'
+    },
+    ability5: {
+        type: 'keypress',
+        keys: '5'
+    },
+    ability6: {
+        type: 'keypress',
+        keys: '6'
+    },
+    ability7: {
+        type: 'keypress',
+        keys: '7'
+    },
+    ability8: {
+        type: 'keypress',
+        keys: '8'
+    },
+    ability9: {
+        type: 'keypress',
+        keys: '9'
+    },
+    ability10: {
+        type: 'keypress',
+        keys: '0'
+    },
+    ability11: {
+        type: 'keypress',
+        keys: '-'
+    },
+    ability12: {
+        type: 'keypress',
+        keys: '='
+    },
+
+    /*
+     * Ability aliases (Actions)
+     */
+    actionCombatSpammable: {
+        type: 'keypress',
+        keys: '2'
+    },
+    actionHealTarget: {
+        type: 'keypress',
+        keys: '3'
+    },
+    actionDotTarget: {
+        type: 'keypress',
+        keys: '4'
+    },
+    actionNukeTarget: {
+        type: 'keypress',
+        keys: '5'
+    },
+    // break
+    actionBuffTargetShort: {
+        type: 'keypress',
+        keys: '8'
+    },
+    actionBuffTargetLong: {
+        type: 'keypress',
+        keys: '9'
+    },
+    actionBuffGroup: {
+        type: 'keypress',
+        keys: '0'
     }
 };
--- run.js
+++ run.js
@@ -1,28 +1,8 @@
 let bot = require('./lib/bot.js');
 
 let nodebot = new bot({
-    automations: {
-        spamOnTarget: {
-            watchers: {
-                colorWatcher: {
-                    type: 'pixel',
-                    compareMethod: 'colorEqual',
-                    color: {
-                        r: 255,
-                        g: 255,
-                        b: 255,
-                        a: 255
-                    },
-                    x: 1190,
-                    y: 429
-                }
-            },
-            actions: [
-                'actionOne'
-            ]
-        }
-    },
-    actions: require('actions.js'),
+    automations: {},
+    actions: require('./actions.js'),
 });
 
 nodebot.start();
